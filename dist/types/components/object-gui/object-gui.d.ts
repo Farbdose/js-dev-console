6b63b0b34ecd2aaacb8f278c5b2eee8505cf1908
Fabian Nitsche <fabian.nitsche@tu-ilmenau.de>
2018-06-06 16:05:20 +0200

    fix line break

--- dist/types/components/object-gui/object-gui.d.ts
+++ dist/types/components/object-gui/object-gui.d.ts
@@ -1,4 +1,7 @@
 import '../../stencil.core';
+import '../../stencil.core';
+import '../../stencil.core';
+import '../../stencil.core';
 export declare class ObjectGui {
     parent: any;
     excludeProto: any;
